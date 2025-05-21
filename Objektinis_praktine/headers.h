3ac95a72122996fe8fb18ebddea2ecc4d603e3d5
Specialistas13 <emilis.slabada@gmail.com>
2025-05-21 11:10:25 +0300

    1 uzd

---
+++ Objektinis_praktine/headers.h
@@ -0,0 +1,28 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <locale>
+#include <clocale>
+#include <algorithm>
+#include <cctype>
+#include <unicode/unistr.h>
+#include <unicode/ustream.h>
+#include <unicode/uchar.h>
+#include <regex>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::stringstream;
+using icu::UnicodeString;
+
+#endif
